b125da09653bfcdbaf89b9939fe60aeeffee5c99
Damich <m.aikel150499@gmail.com>
2020-10-13 23:02:36 -0400

    Configuracion de la base de datos

--- pTiendaOnlineDesk/settings.py
+++ pTiendaOnlineDesk/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pTiendaOnlineDesk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'articulosClientes',
+        'USER':'postgres',
+        'PASSWORD':'daMurq.15',
+        'HOST': 'localhost',
+        'DATABASE_PORT': '5432',
     }
 }
 
