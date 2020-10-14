5569ca3c8f6cd5aedb2801da74c173ec01eaf55f
Damich <m.aikel150499@gmail.com>
2020-10-14 17:57:03 -0400

    Primera modificacion en el panel de control

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -5,13 +5,16 @@ from django.db import models
 class Clientes(models.Model):
     nombre=models.CharField(max_length=30)
     direccion=models.CharField(max_length=50)
-    email=models.EmailField()
+    email=models.EmailField(blank=True,null=True)
     tlfno=models.CharField(max_length=7)
 
 class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
     seccion=models.CharField(max_length=20)
     precio=models.IntegerField()
+    def __str__(self):
+        return '%s tiene un precio de %s y pertenece a la seccion %s' %(self.nombre,self.precio,self.seccion)
+
 
 class Pedidos(models.Model):
     numero=models.IntegerField()
